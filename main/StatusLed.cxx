b643107f931323f40d594319738b973e6c3b4249
MG-5 <mg-95@t-online.de>
2022-09-07 14:26:42 +0200

    Add hardware PWM for status LED

--- main/StatusLed.cxx
+++ main/StatusLed.cxx
@@ -1,33 +1,63 @@
 #include "StatusLed.hpp"
 
+#include "helpers/freertos.hpp"
+
 void StatusLed::taskMain()
 {
-    ledGpio.init(gpio_mode_t::GPIO_MODE_OUTPUT);
+    initLedPwm();
 
     while (true)
     {
         if (!isConnected)
         {
-            ledGpio.write(true);
-            vTaskDelay(pdMS_TO_TICKS(500));
+            setLedDuty(100);
+            vTaskDelay(toOsTicks(300.0_ms));
 
-            ledGpio.write(false);
-            vTaskDelay(pdMS_TO_TICKS(500));
+            setLedDuty(0);
+            vTaskDelay(toOsTicks(300.0_ms));
         }
         else if (pulseArrived)
         {
-            ledGpio.write(true);
-            vTaskDelay(pdMS_TO_TICKS(1000));
-
-            ledGpio.write(false);
+            setLedDuty(50);
+            vTaskDelay(pdMS_TO_TICKS(700));
             pulseArrived = false;
         }
         else
         {
-            ledGpio.write(true);
-            vTaskDelay(pdMS_TO_TICKS(1));
-            ledGpio.write(false);
-            vTaskDelay(pdMS_TO_TICKS(15));
+            setLedDuty(5);
+            vTaskDelay(toOsTicks(50.0_ms));
         }
     }
+}
+
+//--------------------------------------------------------------------------------------------------
+void StatusLed::initLedPwm()
+{
+    // Prepare and then apply the LEDC PWM timer configuration
+    ledc_timer_config_t ledcTimer = {.speed_mode = PwmMode,
+                                     .duty_resolution = PwmResolution,
+                                     .timer_num = LEDC_TIMER_0,
+                                     .freq_hz = 5000, // frequency in Hz
+                                     .clk_cfg = LEDC_AUTO_CLK};
+    ESP_ERROR_CHECK(ledc_timer_config(&ledcTimer));
+
+    // Prepare and then apply the LEDC PWM channel configuration
+    ledc_channel_config_t ledcChannel = {.gpio_num = settings::StatusLedGpio,
+                                         .speed_mode = PwmMode,
+                                         .channel = LedChannel,
+                                         .intr_type = LEDC_INTR_DISABLE,
+                                         .timer_sel = LEDC_TIMER_0,
+                                         .duty = 0, // Set duty to 0%
+                                         .hpoint = 0,
+                                         .flags = {0}};
+    ESP_ERROR_CHECK(ledc_channel_config(&ledcChannel));
+}
+
+//--------------------------------------------------------------------------------------------------
+void StatusLed::setLedDuty(uint8_t percentage)
+{
+    const uint32_t Duty = uint32_t(percentage) * uint32_t(MaximumDuty) / 100;
+
+    ledc_set_duty(PwmMode, LedChannel, Duty);
+    ledc_update_duty(PwmMode, LedChannel);
 }
